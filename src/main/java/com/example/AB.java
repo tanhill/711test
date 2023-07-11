f2e0462d9b7199bf5a190cec3504dcea36e9e2f7
hill <1332427359@qq.com>
2023-07-11 15:41:37 +0800

    ???

--- src/main/java/com/example/AB.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class AB {
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/com/example/sq.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class sq {
-    public static void main(String[] args) {
-        System.out.println("#######");
-    }
-}
